feb941e60a3492ab888d15232647f2effaf1a09d
elitenomad <stalin.pranava@gmail.com>
2015-11-21 09:40:47 +1100

    Introduce a Router - Discover Meteor

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,19 +1,3 @@
-var postsData = [
-	{
-		title: 'Title 1',
-		url: 'http://google.com.au'
-	},
-	{
-		title: 'Agtrader',
-		url: 'http://agtrader.com.au'
-	},
-	{
-		title: 'Movie',
-		url: 'http://imdb.com'
-	}
-]
-
-
 Template.postsList.helpers({
-	posts: postsData
+	posts: Posts.find({})
 })
